3cc347853e75899fcba7c109e61681af636db86b
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-04-10 06:35:15 +0300

    refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,15 @@ import { Suspense } from 'react';
 import { Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
-import ErrorPinotify from './ErrorPinotify/ErrorPinotify';
+import { Pinotify } from './Pinotify';
 import { PublicRoute, PrivateRoute } from './common/Routes';
 import Layout from './Layout/Layout';
 import { Header } from './Header';
 import { getMessage } from '../store/selectors/authSelectors';
 import { routes } from '../routes';
 // import { token } from '../utils/apiUtils';
-import fade from './ErrorPinotify/ErrorPinotify.module.css';
 // import SimpleTest from './SimpleTest/SimpleTest';
+import fade from './Pinotify/Pinotify.module.css';
 
 export default function App() {
   const message = useSelector(getMessage);
@@ -18,7 +18,7 @@ export default function App() {
     <Layout>
       <Header />
       <CSSTransition in={message} classNames={fade} timeout={250} unmountOnExit>
-        <ErrorPinotify message={message} />
+        <Pinotify message={message} />
       </CSSTransition>
       <Suspense fallback={false}>
         <Switch>
--- src/components/ErrorPinotify/ErrorPinotify.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './ErrorPinotify.module.css';
-
-export default function ErrorPinotify({ message }) {
-  return <div className={styles.infoMessage}>{message}</div>;
-}
