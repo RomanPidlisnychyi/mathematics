df140d4a322176291b4a83d94407e1d8d1a15373
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-05-20 21:06:43 +0300

    reversed testing result

--- src/store/selectors/testingSelectors.js
+++ src/store/selectors/testingSelectors.js
@@ -4,8 +4,13 @@ import { preperedResults } from '../../utils/preperedResults';
 export const getTesting = state => state.testing;
 export const getTestingResults = state => state.testingResults;
 
-export const getUpdatedTestingResults = createSelector(
+export const getReversedTestingResults = createSelector(
   getTestingResults,
+  testingResults => [...testingResults].reverse()
+);
+
+export const getUpdatedTestingResults = createSelector(
+  getReversedTestingResults,
   testingResults => testingResults.map(result => preperedResults(result))
 );
 
